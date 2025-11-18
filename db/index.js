add128fd3916cccbc1cac55090ab00690b8c245c
Hariharan <bphariharan1301@gmail.com>
2025-11-18 21:06:49 +0530

    DB Connect

--- db/index.js
+++ db/index.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = pool;
